38ce08b8827bd56b960f6eb3ebac0f5d8eb316f3
shashank <sarthanur@gmail.com>
2023-05-01 17:28:20 +0530

    Hero Page Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return (
-    <>
-    
-    </>
-  )
-}
+    return (
+        <>
+            <div className="main">
+                <div className="main-left">
+                    <h3>
+                        SKIN <br />
+                        PROTECTION <br />
+                        CREAM
+                    </h3>
+                    <br /><br />
+                    <div>
+                        <h1> Trendy Collection </h1>
+                        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, quae. Amet nesciunt nihil doloribus totam dolorum nobis cumque esse. Quaerat.</p>
+                    </div>
+                </div>
+                <div className="main-center">
+                    <div>
+                        <img src="./images/hero.png" alt="" />
+                    </div>
+                </div>
+                <div className="main-right">
+                    <div>
+                        <h1>1.5m</h1>
+                        <p>Monthly Traffic</p>
+                    </div>
+                    <br /><br /><br /><br />
+                    <div>
+                        <h1>100k</h1>
+                        <p>Happy Customers</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default Header
+export default Header;
