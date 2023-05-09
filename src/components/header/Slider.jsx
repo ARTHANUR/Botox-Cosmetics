0c6bb2f3a3f11428e49f31bdeb6f6aab9fd9ba36
shashank <sarthanur@gmail.com>
2023-05-09 23:10:20 +0530

    Navigation added to the slider

--- src/components/header/Slider.jsx
+++ src/components/header/Slider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 import "./Slider.css";
 
 // import { SliderProducts } from "../../data/products";
@@ -31,36 +31,46 @@ const Slider = () => {
             img: "./images/img3.png",
         },
         {
-            name: "Lips",
+            name: "LIPS",
             detail: "Lip Care",
             price: "69",
             img: "./images/Lipstick.png",
+        },
+        {
+            name: "BLUSH",
+            detail: "Skin Glow",
+            price: "80",
+            img: "./images/blush.png",
+        },
+        {
+            name: "BRUSH",
+            detail: "Even Spreading",
+            price: "22",
+            img: "./images/brush.png",
         },
     ];
 
     return (
         <>
             <div className="s-container">
-                <Swiper slidesPerView={2} spaceBetween={40} slidesPerGroup={1} loop={true}>
+                <Swiper modules={[Pagination, Navigation]} navigation={true} pagination={true} slidesPerView={2} spaceBetween={40} slidesPerGroup={1} loop={true}>
                     {SliderProducts.map((slide, i) => {
                         return (
                             <SwiperSlide>
-                              <div className="slide">
-                              <div className="left">
-                                    <span>
+                                <div className="slide">
+                                    <div className="left">
+                                        <span>
+                                            <h2> {slide.name} </h2>
+                                            <p>{slide.detail}</p>
+                                        </span>
+                                        <h1>$ {slide.price} </h1>
+                                        <button>Shop Now</button>
+                                    </div>
 
-                                        <h2> {slide.name} </h2>
-                                        <p>{slide.detail}</p>
-                                    </span>
-                                    <h1>$ {slide.price} </h1>
-                                    <button>Shop Now</button>
+                                    <div className="right">
+                                        <img src={slide.img} alt="" />
+                                    </div>
                                 </div>
-
-                                <div className="right">
-                                    <img src={slide.img} alt="" />
-                                </div>
-                              </div>
-                                
                             </SwiperSlide>
                         );
                     })}
