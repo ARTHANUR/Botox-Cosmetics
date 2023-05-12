f325021d65577a7b8c404bea26ad41cc1b937589
shashank <sarthanur@gmail.com>
2023-05-12 12:09:41 +0530

    Filter functionality added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,107 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.css";
 
+const ProductsData = [
+    {
+        name: "SKIN",
+        detail: "Super Skin Care",
+        price: "25",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695827/cosmetics/img1_ilui1t.png",
+        type: "skin care",
+    },
+    {
+        name: "SKIN",
+        detail: "Hydrating serum",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img2_f8lr8u.png",
+        type: "skin care",
+    },
+    {
+        name: "NATURE",
+        detail: "Super Skin Care",
+        price: "25",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img3_zx8vf7.png",
+        type: "skin care",
+    },
+    {
+        name: "Foundation",
+        detail: "Super Skin Care",
+        price: "25",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img4_yznyfi.png",
+        type: "foundation",
+    },
+    {
+        name: "CONDITIONER",
+        detail: "Super Skin Care",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img5_nfgj7l.png",
+        type: "conditioner",
+    },
+    {
+        name: "NATURE",
+        detail: "Super Skin Care",
+        price: "25",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img6_z1w8ip.png",
+        type: "skin care",
+    },
+    {
+        name: "CONDITIONER",
+        detail: "Best Conditioner",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img7_gcnr9h.png",
+        type: "conditioner",
+    },
+    {
+        name: "CONDITIONER",
+        detail: "Best Conditioner",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img8_wkigg1.png",
+        type: "conditioner",
+    },
+    {
+        name: "CONDITIONER",
+        detail: "Best Conditioner",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img9_bnva98.png",
+        type: "conditioner",
+    },
+    {
+        name: "CONDITIONER",
+        detail: "Best Conditioner",
+        price: "30",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695826/cosmetics/img10_bhd3wx.png",
+        type: "conditioner",
+    },
+    {
+        name: "FOUNDATION",
+        detail: "Nourish your skin",
+        price: "12",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img11_muecax.png",
+        type: "foundation",
+    },
+    {
+        name: "FOUNDATION",
+        detail: "Nourish your skin",
+        price: "12",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695822/cosmetics/img12_pquptp.png",
+        type: "foundation",
+    },
+    {
+        name: "FOUNDATION",
+        detail: "Nourish your skin",
+        price: "12",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695822/cosmetics/img13_amvme1.png",
+        type: "foundation",
+    },
+    {
+        name: "FOUNDATION",
+        detail: "Nourish your skin",
+        price: "12",
+        img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695825/cosmetics/img14_nnbxy0.png",
+        type: "foundation",
+    },
+];
+
 const Product = () => {
-    const ProductsData = [
-        {
-            name: "SKIN",
-            detail: "Super Skin Care",
-            price: "25",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695827/cosmetics/img1_ilui1t.png",
-            type: "skin care",
-        },
-        {
-            name: "SKIN",
-            detail: "Hydrating serum",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img2_f8lr8u.png",
-            type: "skin care",
-        },
-        {
-            name: "NATURE",
-            detail: "Super Skin Care",
-            price: "25",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img3_zx8vf7.png",
-            type: "skin care",
-        },
-        {
-            name: "Foundation",
-            detail: "Super Skin Care",
-            price: "25",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img4_yznyfi.png",
-            type: "foundation",
-        },
-        {
-            name: "CONDITIONER",
-            detail: "Super Skin Care",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img5_nfgj7l.png",
-            type: "conditioner",
-        },
-        {
-            name: "NATURE",
-            detail: "Super Skin Care",
-            price: "25",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img6_z1w8ip.png",
-            type: "skin care",
-        },
-        {
-            name: "CONDITIONER",
-            detail: "Best Conditioner",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695819/cosmetics/img7_gcnr9h.png",
-            type: "conditioner",
-        },
-        {
-            name: "CONDITIONER",
-            detail: "Best Conditioner",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img8_wkigg1.png",
-            type: "conditioner",
-        },
-        {
-            name: "CONDITIONER",
-            detail: "Best Conditioner",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img9_bnva98.png",
-            type: "conditioner",
-        },
-        {
-            name: "CONDITIONER",
-            detail: "Best Conditioner",
-            price: "30",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695826/cosmetics/img10_bhd3wx.png",
-            type: "conditioner",
-        },
-        {
-            name: "FOUNDATION",
-            detail: "Nourish your skin",
-            price: "12",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695820/cosmetics/img11_muecax.png",
-            type: "foundation",
-        },
-        {
-            name: "FOUNDATION",
-            detail: "Nourish your skin",
-            price: "12",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695822/cosmetics/img12_pquptp.png",
-            type: "foundation",
-        },
-        {
-            name: "FOUNDATION",
-            detail: "Nourish your skin",
-            price: "12",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695822/cosmetics/img13_amvme1.png",
-            type: "foundation",
-        },
-        {
-            name: "FOUNDATION",
-            detail: "Nourish your skin",
-            price: "12",
-            img: "https://res.cloudinary.com/dboa7dqkl/image/upload/v1683695825/cosmetics/img14_nnbxy0.png",
-            type: "foundation",
-        },
-    ];
+    const [menuProduct, setMenuProduct] = useState(ProductsData);
+
+    const filter = (type) => {
+        setMenuProduct(ProductsData.filter((product) => product.type === type));
+    };
+
     return (
         <>
             <div className="product-head">
@@ -111,15 +118,15 @@ const Product = () => {
             <div className="product-section">
                 <div className="item-left">
                     <ul>
-                        <li> All </li>
-                        <li> Skin Care</li>
-                        <li> Conditioner</li>
-                        <li> Foundation</li>
+                        <li onClick={() => setMenuProduct(ProductsData)}> All </li>
+                        <li onClick={() => filter("skin care")}> Skin Care</li>
+                        <li onClick={() => filter("conditioner")}> Conditioner</li>
+                        <li onClick={() => filter("foundation")}> Foundation</li>
                     </ul>
                 </div>
                 <span />
                 <div className="item-container">
-                    {ProductsData.map((product) => {
+                    {menuProduct.map((product) => {
                         return (
                             <>
                                 <div className="product-container">
